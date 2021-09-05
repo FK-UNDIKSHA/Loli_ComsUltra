8225735485ebd06a9efa270a895ee47d97ab9383
Semara Incorporated <semaraincorporated@gmail.com>
2021-09-05 18:04:58 +0800

    Add Serial Communication
    I Added Simple Serial Communication Between Arduino and Python3,
    Firmata is not a good option (overcomplicated)
    
    I also think to use string over serial comms (convenient reason)

--- ultrasonik.ino.ino
+++ ultrasonik.ino.ino
@@ -2,29 +2,40 @@
 #define TRIGPIN 9
 int HT;
 float distance = 0.0;
+String y = "";
 void setup() {
-  Serial.begin(9600);
-  // put your setup code here, to run once:
-  pinMode(ECHOPIN,INPUT);
-  pinMode(TRIGPIN,OUTPUT);
-  //pinMode(led,OUTPUT);
-  delay(1000);
-  HT = 200;
-
+ Serial.begin(9600);
+ pinMode(ECHOPIN,INPUT);
+ pinMode(TRIGPIN,OUTPUT);
+ //pinMode(led,OUTPUT);
+ delay(1000);
+ HT = 200;
+ //Serial.setTimeout(1);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(TRIGPIN,LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIGPIN,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGPIN,LOW);
-  distance = pulseIn(ECHOPIN,HIGH);
-  distance = (distance*0.0343)/2;
-  int body = (181-distance);
-  int H2=HT-distance;
-  Serial.println("pedo");
-  Serial.println(H2);
+  while (Serial.available()){
+    String x = Serial.readString();
+    //Serial.println(x);
+    x.trim();
+   y = y+x;
+  }
 
+  //Serial.print(y);
+  if (y == "tinggi"){
+    digitalWrite(TRIGPIN,LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGPIN,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGPIN,LOW);
+    distance = pulseIn(ECHOPIN,HIGH);
+    distance = (distance*0.0343)/2;
+    int body = (181-distance);
+    int H2=HT-distance;
+    delay(3000);
+    Serial.print(H2);
+    y = "";
+    H2 = -1;
+  }
+  else{
+  y="";}
 }
