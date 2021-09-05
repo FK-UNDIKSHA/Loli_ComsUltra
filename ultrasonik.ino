8778541dd95d8406a701942167c86f6cb2c4dbdc
Semara Incorporated <semaraincorporated@gmail.com>
2021-09-06 02:26:47 +0800

    Add More Command and Fixing Delay Problem
    I changed the delay value to make send value faster (need more testing in the future)
    For Command System, i know there are many security flaw here, but my purpose is for prototyping in tight schedule
    
    if you want to fix, go a head :D
    ill be happy if you are willing to help :D

--- ultrasonik.ino
+++ ultrasonik.ino
@@ -1,6 +1,8 @@
 #define ECHOPIN 10
 #define TRIGPIN 9
 int HT;
+int state_ultra = 0;
+String berat;
 float distance = 0.0;
 String y = "";
 void setup() {
@@ -8,7 +10,7 @@ void setup() {
  pinMode(ECHOPIN,INPUT);
  pinMode(TRIGPIN,OUTPUT);
  //pinMode(led,OUTPUT);
- delay(1000);
+ //delay(1000);
  HT = 200;
  //Serial.setTimeout(1);
 }
@@ -31,11 +33,28 @@ void loop() {
     distance = (distance*0.0343)/2;
     int body = (181-distance);
     int H2=HT-distance;
-    delay(3000);
+    //delay(3000);
     Serial.print(H2);
     y = "";
     H2 = -1;
   }
+  // i know this potentially buffer overflow and other bugs, (REMEMBER THIS IS PROTOTYPE)
+  else if (y.startsWith("pesan;")){
+    //do parsing
+    for (int l=0;l<=y.length();l++){
+      //Serial.println(y[l]);
+      if(String(y[l]) == ";"){
+        //Serial.print("YA");
+        for(int k=l+1;k<=y.length();k++){
+            berat = berat + String(y[k]);
+            //berat.trim();
+          }
+          Serial.print(berat);
+          break;
+        }
+       }
+       y="";
+     }
   else{
   y="";}
 }
